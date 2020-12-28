383f9f068349d4f14e3bacddd7b75ec836a9e5b8
samyak-jain <samtan106@gmail.com>
2020-12-28 12:18:25 +0530

    Fixed image loading in episode screen

--- app/src/main/java/dev/samyak/unagi/compose/layout/EpisodeListScreen.kt
+++ app/src/main/java/dev/samyak/unagi/compose/layout/EpisodeListScreen.kt
@@ -4,31 +4,36 @@ import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.text.Html
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumnForIndexed
+import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.AmbientContentAlpha
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.collection.MutableVector
-import androidx.compose.runtime.collection.mutableVectorOf
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.AmbientContext
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
+import com.facebook.shimmer.ShimmerFrameLayout
 import dev.samyak.core.data.Episode
 import dev.samyak.unagi.viewmodels.EpisodeScreenModel
 import java.util.*
-import kotlin.collections.HashMap
 
 @Composable
 fun EpisodePage(navController: NavController, episodeScreenModel: EpisodeScreenModel, showId: Int) {
@@ -58,7 +63,7 @@ fun EpisodePage(navController: NavController, episodeScreenModel: EpisodeScreenM
             bitmap?.asImageBitmap()?.let {
                 Image(bitmap = it,
                     modifier = Modifier.fillMaxWidth().height(180.dp),
-                    contentScale = ContentScale.Crop)
+                    contentScale = ContentScale.FillHeight)
             }
 
             Text(text = showDataValue.title.capitalize(Locale.getDefault()),
@@ -77,45 +82,85 @@ fun EpisodePage(navController: NavController, episodeScreenModel: EpisodeScreenM
 
 @Composable
 fun AboutPage(description: String) {
-    Text(text = "Description", style = MaterialTheme.typography.subtitle1)
-    Text(text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        Html.fromHtml(description, Html.FROM_HTML_MODE_LEGACY).toString()
-    } else {
-        @Suppress("DEPRECATION")
-        Html.fromHtml(description).toString()
-    }, style = MaterialTheme.typography.body1)
+    Column(modifier = Modifier.fillMaxHeight().padding(horizontal = 8.dp)) {
+        Text(text = "Description", style = MaterialTheme.typography.subtitle1)
+        Text(text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Html.fromHtml(description, Html.FROM_HTML_MODE_LEGACY).toString()
+        } else {
+            @Suppress("DEPRECATION")
+            Html.fromHtml(description).toString()
+        }, style = MaterialTheme.typography.body1,
+        overflow = TextOverflow.Ellipsis)
+    }
 }
 
 @Composable
 fun EpisodeScrollView(episodeList: List<Episode>) {
+    LazyColumnFor(items = episodeList, modifier = Modifier.fillMaxHeight().fillMaxWidth()) { item ->
+        Box(modifier = Modifier.fillMaxWidth().height(150.dp)) {
+            item.thumbnail?.let { LoadImage(url = it) }
 
-    val thumbnailList: MutableList<Bitmap> = remember { mutableListOf() }
-
-
-    LazyColumnForIndexed(items = episodeList) { index, item ->
-
-        Glide.with(AmbientContext.current).asBitmap()
-            .load(item.thumbnail).into(object : CustomTarget<Bitmap>() {
-                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                    thumbnailList[index] = resource
-                }
-
-                override fun onLoadCleared(placeholder: Drawable?) {}
-
-            })
-
-        thumbnailList.getOrNull(index)?.let {
-            Box(modifier = Modifier.fillMaxWidth().height(100.dp)) {
-                Image(
-                    bitmap = it.asImageBitmap(),
-                    modifier = Modifier.fillMaxWidth().fillMaxHeight(),
-                    contentScale = ContentScale.Crop
-                )
-
-                Providers(AmbientContentAlpha provides ContentAlpha.medium) {
-                    Text(text = item.name, modifier = Modifier.align(Alignment.CenterStart), color = MaterialTheme.colors.background)
-                }
+            Providers(AmbientContentAlpha provides ContentAlpha.medium) {
+                Text(text = item.name, modifier = Modifier.align(Alignment.BottomStart))
             }
         }
+    }
+}
+
+@Composable
+fun LoadImage(url: String, modifier: Modifier = Modifier) {
+    var image by remember { mutableStateOf<ImageBitmap?>(null) }
+    var drawable by remember { mutableStateOf<Drawable?>(null) }
+    val sizeModifier = modifier.fillMaxWidth().fillMaxHeight()
+    val context = AmbientContext.current
+
+    onCommit(url) {
+        val glide = Glide.with(context)
+        val target = object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                image = resource.asImageBitmap()
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+                image = null
+                drawable = placeholder
+            }
+        }
+
+        glide.asBitmap().load(url).centerCrop().diskCacheStrategy(DiskCacheStrategy.ALL).into(target)
+
+        onDispose {
+            image = null
+            drawable = null
+            glide.clear(target)
+        }
+    }
+
+    val theImage = image
+    val theDrawable = drawable
+
+    if (theImage != null) {
+        Column(modifier = sizeModifier) {
+            Image(bitmap = theImage, modifier = sizeModifier, contentScale = ContentScale.Crop)
+        }
+    } else if (theDrawable != null) {
+        Canvas(modifier = sizeModifier) {
+            drawIntoCanvas { canvas ->
+                theDrawable.draw(canvas.nativeCanvas)
+            }
+        }
+    }
+}
+
+@Composable
+fun ShimmerView() {
+    val context = AmbientContext.current
+
+    val customView = remember {
+        ShimmerFrameLayout(context)
+    }
+
+    AndroidView({ customView }, modifier = Modifier.fillMaxWidth().fillMaxHeight()) { view ->
+        view.startShimmer()
     }
 }
